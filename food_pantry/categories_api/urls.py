8857858b2390122912277cec54e9c02226f41a9d
Arowi <aroamateogomez1995@gmail.com>
2025-07-28 10:53:58 +0200

    Fix some errors with the categories_api #12

--- food_pantry/categories_api/urls.py
+++ food_pantry/categories_api/urls.py
@@ -5,6 +5,6 @@ from .views.categories_view import CategoryListCreateView, CategoryDetailView
 base_url=""  
     
 urlpatterns = [  
-    path(base_url, CategoryListCreateView.as_view(), name="donors"),  
-    path(base_url + '<int:pk>', CategoryDetailView.as_view(), name="donor"),  
+    path(base_url, CategoryListCreateView.as_view(), name="categories"),  
+    path(base_url + '<int:pk>', CategoryDetailView.as_view(), name="category"),  
 ]
